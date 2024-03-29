ed1549d9facd38507dfc94ed63d1d5a3388b1ddc
Lorenzo Maturano <maturanolorenzo@gmail.com>
2024-03-29 16:08:38 -0300

    feat: moving more stuff to env file

--- readme.md
+++ readme.md
@@ -6,7 +6,7 @@ Generate the post md file for your bitdevs meetup using Github issues.
 
 1. To use the Github API you'll need to follow these [instructions](https://docs.github.com/en/rest/quickstart).
 2. Create a .env file following the .env.sample
-3. Make sure to have your issues in the following format:
+3. Make sure to have your issues in the following format in your dotfiles repo:
 
 ```
 <title>
@@ -16,5 +16,5 @@ Generate the post md file for your bitdevs meetup using Github issues.
 # Usage
 
 ```
-cargo run -- --org-name=lorenzolfm --repo-name=floripabitdevs --issue-number=12 --meetup-date=2024-01-01 --meetup-number=23 --meetup-link=google.com
+cargo run -- --issue-number=12 --meetup-date=2024-01-01 --meetup-number=23 --meetup-link=meetup.com/saopaulobitdevs/69420
 ```
--- src/main.rs
+++ src/main.rs
@@ -30,27 +30,36 @@ struct Args {
     meetup_link: String,
 
     #[arg(long)]
-    org_name: String,
-    #[arg(long)]
-    repo_name: String,
-    #[arg(long)]
     issue_number: u16,
 }
 
 fn main() -> Result<(), reqwest::Error> {
+    dotenv().ok();
+
+    let meetup_name =
+        std::env::var("MEETUP_NAME").expect("PROJECT_NAME environment variable not set");
+    let meetup_chat_link =
+        std::env::var("MEETUP_CHAT_LINK").expect("PROJECT_NAME environment variable not set");
+
+    let gh_api_project_name =
+        std::env::var("GH_API_PROJECT_NAME").expect("PROJECT_NAME environment variable not set");
+    let gh_api_token =
+        std::env::var("GH_API_TOKEN").expect("GITHUB_TOKEN environment variable not set");
+
+    let repo_org = std::env::var("REPO_ORG").expect("GITHUB_TOKEN environment variable not set");
+    let repo_name = std::env::var("REPO_NAME").expect("GITHUB_TOKEN environment variable not set");
+
     let Args {
         meetup_number,
         meetup_date,
         meetup_link,
-        org_name,
-        repo_name,
         issue_number,
     } = Args::parse();
 
     let post_prefix = format!(
         "---\nlayout: post\ntype: socratic\ntitle: \"Seminário Socrático {}\"\nmeetup: {}\n---\n\n\
         ## Avisos\n\n\
-        - Entrem no grupo do Whatsapp [São Paulo Bitdevs](https://chat.whatsapp.com/HiaPqjmUqER5djFPR1Yl3T)!\n\
+        - Entrem no grupo do Whatsapp [{meetup_name}]({meetup_chat_link})!\n\
         - Respeite a privacidade dos participantes.\n\
         - Os meetups nunca são gravados. Queremos todos a vontade para participar e discutir os assuntos programados, de forma anônima se assim o desejarem.\n\n\
         ## Agradecimentos\n\n\
@@ -59,23 +68,19 @@ fn main() -> Result<(), reqwest::Error> {
         meetup_number, meetup_link
     );
 
-    dotenv().ok();
-
-    let project_name =
-        std::env::var("PROJECT_NAME").expect("PROJECT_NAME environment variable not set");
-    let github_token =
-        std::env::var("GITHUB_TOKEN").expect("GITHUB_TOKEN environment variable not set");
-
     let client = Client::new();
     let mut headers = HeaderMap::new();
 
-    headers.insert(USER_AGENT, HeaderValue::from_str(&project_name).unwrap());
-    headers.insert(AUTHORIZATION, HeaderValue::from_str(&github_token).unwrap());
+    headers.insert(
+        USER_AGENT,
+        HeaderValue::from_str(&gh_api_project_name).unwrap(),
+    );
+    headers.insert(AUTHORIZATION, HeaderValue::from_str(&gh_api_token).unwrap());
     headers.insert(ACCEPT, HeaderValue::from_static(ACCEPT_VALUE));
 
     let response = client
         .get(format!(
-            "{API_URL}/repos/{org_name}/{repo_name}/issues/{issue_number}",
+            "{API_URL}/repos/{repo_org}/{repo_name}/issues/{issue_number}",
         ))
         .headers(headers.clone())
         .send()?;
